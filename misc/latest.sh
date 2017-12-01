1adb1f6e791740bd7f6f4e9c5fcf27a9bca88467
Pierre Fenoll <pierrefenoll@gmail.com>
2017-12-01 18:42:17 +0100

    har-ry: be less picky in taget path selection

--- misc/latest.sh
+++ misc/latest.sh
@@ -7,25 +7,34 @@ set -o nounset
 
 slug=CoveredCI/testman
 
-target_path=/usr/local/bin
-case :$PATH: in
-    *:$target_path:*) ;;
-    *)
-        echo $target_path' is not in your PATH'
-        exit 2
-        ;;
-esac
-
-echo Looking for latest tag of $slug
-latest_tag_url=$(curl --silent --location --output /dev/null --write-out '%{url_effective}' https://github.com/$slug/releases/latest)
-latest_tag=$(basename "$latest_tag_url")
-echo "Latest tag: $latest_tag"
-
 fatal() {
     echo "$@"
     echo "Please report this at https://github.com/$slug/issues or hi@coveredci.co"
     exit 2
 }
+
+# Note: ~/.local/bin is for TravisCI.com
+target_path=
+for path in /usr/local/bin /usr/bin ~/.local/bin nil; do
+    case :$PATH: in
+        :nil:) fatal "Could not find a suitable target path among $PATH" ;;
+        *:$path:*)
+            mkdir -p $path >/dev/null 2>&1 || true
+            if touch $path/testman >/dev/null 2>&1; then
+                target_path=$path
+                echo "Selected target path: $target_path"
+                break
+            else
+                rm $path/testman >/dev/null 2>&1 || true
+            fi;;
+        *) ;;
+    esac
+done
+
+echo Looking for latest tag of $slug
+latest_tag_url=$(curl --silent --location --output /dev/null --write-out '%{url_effective}' https://github.com/$slug/releases/latest)
+latest_tag=$(basename "$latest_tag_url")
+echo "Latest tag: $latest_tag"
 
 exe=
 case "$(uname -s)" in  # https://stackoverflow.com/a/27776822/1418165
@@ -39,7 +48,7 @@ echo "Downloading $exe v$latest_tag"
 tmp="$(mktemp)"
 curl -# --location --output "$tmp" "https://github.com/$slug/releases/download/$latest_tag/$exe"
 chmod +x "$tmp"
-mv --verbose "$tmp" $target_path/testman
+mv "$tmp" $target_path/testman
 
 if ! which testman >/dev/null 2>&1; then
     fatal "$exe does not appear to be in $target_path"
