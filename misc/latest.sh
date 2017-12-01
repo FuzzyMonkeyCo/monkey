b9f7cc07b54521ae5f5cf1005a70bdd952ab6c8b
Pierre Fenoll <pierrefenoll@gmail.com>
2017-12-01 17:41:56 +0100

    har-ry: enhancements to misc/latest.sh

--- misc/latest.sh
+++ misc/latest.sh
@@ -1,4 +1,7 @@
-#!/bin/sh -e
+#!/bin/sh
+
+set -o errexit
+set -o nounset
 
 # Installs the latest version of https://github.com/CoveredCI/testman for your system
 
@@ -18,22 +21,30 @@ latest_tag_url=$(curl --silent --location --output /dev/null --write-out '%{url_
 latest_tag=$(basename "$latest_tag_url")
 echo "Latest tag: $latest_tag"
 
-exe=''
-case $(uname) in
-    *Linux) exe=testman-linux_amd64 ;;
-    *Darwin) exe=testman-darwin_amd64 ;;
-    *)
-        echo "Unsupported architecture $(uname)"
-        echo "Please open an issue at https://github.com/$slug/issues"
-        exit 2
-        ;;
+fatal() {
+    echo "$@"
+    echo "Please report this at https://github.com/$slug/issues or hi@coveredci.co"
+    exit 2
+}
+
+exe=
+case "$(uname -s)" in  # https://stackoverflow.com/a/27776822/1418165
+    Linux)  exe=testman-linux_amd64 ;;
+    Darwin) exe=testman-darwin_amd64 ;;
+    CYGWIN*|MINGW32*|MSYS*) exe=testman-windows_amd64.exe ;;
+    *) fatal "Unsupported architecture '$(uname -s)': $(uname -a)" ;;
 esac
 
-echo "Downloading v$latest_tag of $exe"
-tmp=/tmp/testman
-curl --silent --location --output $tmp "https://github.com/$slug/releases/download/$latest_tag/$exe"
-chmod +x $tmp
-mv --verbose $tmp $target_path/
+echo "Downloading $exe v$latest_tag"
+tmp="$(mktemp)"
+curl -# --location --output "$tmp" "https://github.com/$slug/releases/download/$latest_tag/$exe"
+chmod +x "$tmp"
+mv --verbose "$tmp" $target_path/testman
 
-testman --version
+if ! which testman >/dev/null 2>&1; then
+    fatal "$exe does not appear to be in $target_path"
+fi
+if ! testman --version | grep "$latest_tag" >/dev/null 2>&1; then
+    fatal "This is not the expected version: $(testman --version || true)"
+fi
 echo Successful installation!
