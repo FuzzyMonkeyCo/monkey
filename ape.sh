8155a87968336e87c5d90b49c3a8a5230aab9d39
Pierre Fenoll <pierrefenoll@gmail.com>
2018-01-23 00:21:21 +0100

    U: shellcheck

--- ape.sh
+++ ape.sh
@@ -7,15 +7,16 @@ err=/tmp/.monkey_$RANDOM.code
 
 i=0
 if ls "$dir"/*.cov >/dev/null 2>&1; then
-    biggest=$(ls "$dir"/*.cov | sort -Vr | head -n1)
-    i=$(basename $biggest .cov)
+    biggest=$(find "$dir" -name '*.cov' | sort -Vr | head -n1)
+    i=$(basename "$biggest" .cov)
     ((i+=1))
 fi
 
-MONKEY_CODEFILE=$err MONKEY_ARGS="$@" "$dir"/monkey.test \
-           -test.coverprofile="$dir"/$i.cov \
-           -test.run=^TestCov$
+MONKEY_CODEFILE=$err MONKEY_ARGS="$*" "$dir"/monkey.test \
+               -test.coverprofile="$dir"/$i.cov \
+               -test.run=^TestCov$
 
 code=$(cat $err)
 rm $err
+# shellcheck disable=SC2086
 exit $code
