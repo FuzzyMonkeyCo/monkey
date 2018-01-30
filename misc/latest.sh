5517fa57afc5f5347d9550f88c643baaab16dff5
Pierre Fenoll <pierrefenoll@gmail.com>
2018-01-30 12:25:56 +0100

    race: fix PATH typo

--- misc/latest.sh
+++ misc/latest.sh
@@ -17,8 +17,8 @@ fatal() {
 # Note: ~/.local/bin is for TravisCI.com
 # Note: C:\Program Files\Git\usr\bin is for appveyor.com
 target_path=
-for path in "$@" /usr/local/bin /usr/bin ~/.local/bin 'C:\Program Files\Git\usr\bin'; do
-    case :"$path": in
+for path in "$@" 'C:/Program Files/Git/usr/bin' /usr/local/bin /usr/bin ~/.local/bin; do
+    case :"$PATH": in
         *:"$path":*)
             if ! mkdir -p "$path" >/dev/null 2>&1; then
                 continue
