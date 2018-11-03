a1b3a4e7af3bfda92944fb15b1098ad949d320ff
Pierre Fenoll <pierrefenoll@gmail.com>
2018-11-03 06:36:13 +0100

    ws work ok so far

--- do_fuzz.go
+++ do_fuzz.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"bytes"
+	"errors"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
-	"os"
-	"os/signal"
+	"sync"
 	"time"
 
 	"github.com/golang/protobuf/proto"
@@ -62,44 +62,22 @@ func fuzzNext(cfg *UserCfg, cmd someCmd) (someCmd someCmd, err error) {
 }
 
 func fuzzNew(cfg *UserCfg, payload []byte) (rep []byte, err error) {
-
-	doDaDING() ///FIXME
-
-	r, err := http.NewRequest(http.MethodPut, apiFuzzNew, bytes.NewBuffer(payload))
-	if err != nil {
-		log.Println("[ERR]", err)
+	if err = newWS(cfg); err != nil { ///FIXME
 		return
 	}
 
-	r.Header.Set(headerContentType, mimeYAML)
-	r.Header.Set(headerAccept, mimeJSON)
-	r.Header.Set(headerUserAgent, binTitle)
-	r.Header.Set(headerXAuthToken, cfg.AuthToken)
-
-	log.Printf("[DBG] 🡱  PUT %s\n  🡱  %dB\n", apiFuzzNew, len(payload))
-	start := time.Now()
-	resp, err := clientUtils.Do(r)
-	log.Printf("[DBG] ❙ %dμs\n", time.Since(start)/time.Microsecond)
-	if err != nil {
+	if rep, err = ws.call(payload); err != nil {
 		// if here probably a HomeConnectionError
-		log.Println("[ERR]", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	if rep, err = ioutil.ReadAll(resp.Body); err != nil {
-		log.Println("[ERR]", err)
-		return
-	}
-	log.Printf("[DBG]\n  🡳  %s\n", rep)
-
-	if resp.StatusCode != http.StatusCreated {
-		err = newStatusError(http.StatusCreated, resp.Status)
-		log.Println("[ERR]", err)
 		return
 	}
 
-	cfg.AuthToken = resp.Header.Get(headerXAuthToken)
+	// if resp.StatusCode != http.StatusCreated {
+	// 	err = newStatusError(http.StatusCreated, resp.Status)
+	// 	log.Println("[ERR]", err)
+	// 	return
+	// }
+
+	// cfg.AuthToken = resp.Header.Get(headerXAuthToken)
 	return
 }
 
@@ -143,60 +121,132 @@ func nextPOST(cfg *UserCfg, payload []byte) (rep []byte, err error) {
 
 ///////////
 
-func doDaDING() {
-	interrupt := make(chan os.Signal, 1)
-	signal.Notify(interrupt, os.Interrupt)
+var ws *wsState
 
-	u := url.URL{Scheme: "ws", Host: "localhost:1042", Path: "/fuzz"}
+type wsState struct {
+	req  chan []byte
+	rep  chan []byte
+	err  chan error
+	ping chan struct{}
+	done chan struct{}
+}
+
+func (ws *wsState) call(payload []byte) (rep []byte, err error) {
+	// FIXME: take a Writer, return a Reader
+	log.Printf("[DBG] 🡱 PUT %dB\n", len(payload))
+	start := time.Now()
+	ws.req <- payload
+	select {
+	case rep = <-ws.rep:
+		log.Println("[DBG] 🡳", time.Now().Sub(start), rep[:4])
+		return
+	case err = <-ws.err:
+		log.Println("[DBG] 🡳", time.Now().Sub(start), err)
+		return
+	case <-time.After(15 * time.Second):
+		err = errors.New("ws call timeout")
+		log.Println("[ERR]", err)
+		return
+	}
+}
+
+func newWS(cfg *UserCfg) error {
+	headers := http.Header{
+		headerUserAgent:  {binTitle},
+		headerXAuthToken: {cfg.AuthToken},
+	}
+	u := &url.URL{Scheme: "ws", Host: "localhost:1042", Path: "/1/fuzz"}
+
 	log.Printf("connecting to %s", u.String())
-
-	headers := http.Header{headerUserAgent: {binTitle}, headerXAuthToken: {"bla"}}
 	c, _, err := websocket.DefaultDialer.Dial(u.String(), headers)
 	if err != nil {
-		log.Fatal("dial:", err)
+		log.Println("[ERR]", err)
+		return err
 	}
-	defer c.Close()
 
-	done := make(chan struct{})
+	ws = &wsState{
+		req:  make(chan []byte, 1),
+		rep:  make(chan []byte, 1),
+		err:  make(chan error, 1),
+		ping: make(chan struct{}, 1),
+		done: make(chan struct{}, 1),
+	}
 
 	go func() {
-		defer close(done)
+		defer func() { ws.done <- struct{}{} }()
 		for {
-			_, message, err := c.ReadMessage()
+			_, rep, err := c.ReadMessage()
 			if err != nil {
-				log.Println("read:", err)
+				log.Println("[ERR]", err)
+				ws.err <- err
 				return
 			}
-			log.Printf("recv: %s", message)
+			log.Printf("recv %dB: %s...\n", len(rep), rep[:4])
+			if len(rep) == 4 && string(rep) == `PING` {
+				ws.ping <- struct{}{}
+			} else {
+				ws.rep <- rep
+			}
+		}
+		log.Println("ws reader ending")
+	}()
+
+	var once sync.Once
+	pong := make(chan struct{}, 1)
+	srvTimeout := 15 * time.Second
+
+	go func() {
+		defer close(ws.req)
+		defer close(ws.rep)
+		defer close(ws.err)
+		defer close(ws.ping)
+		defer close(ws.done)
+
+		func() {
+			for {
+				select {
+				case data := <-ws.req:
+					log.Println("ws.req")
+					if err := c.WriteMessage(websocket.BinaryMessage, data); err != nil {
+						log.Println("[ERR]", err)
+						ws.err <- err
+						return
+					}
+				case <-ws.ping:
+					log.Println("ws.ping")
+					data := []byte(`PONG`)
+					if err := c.WriteMessage(websocket.TextMessage, data); err != nil {
+						log.Println("[ERR]", err)
+						ws.err <- err
+						return
+					}
+					once.Do(func() { pong <- struct{}{} })
+				case <-ws.done:
+					log.Println("ws.done")
+					return
+				case <-time.After(srvTimeout):
+					log.Fatalln("[ERR] srvTimeout")
+					return
+				}
+			}
+		}()
+		log.Println("ws manager ending")
+		if err := c.Close(); err != nil {
+			log.Println("[ERR]", err)
 		}
 	}()
 
-	ticker := time.NewTicker(1 * time.Second)
-	defer ticker.Stop()
-
-	for {
-		select {
-		case <-done:
-			return
-		case t := <-ticker.C:
-			if err := c.WriteMessage(websocket.TextMessage, []byte(t.String())); err != nil {
-				log.Println("write:", err)
-				return
-			}
-		case <-interrupt:
-			log.Println("interrupt")
-
-			// Cleanly close the connection by sending a close message and then
-			// waiting (with timeout) for the server to close the connection.
-			if err := c.WriteMessage(websocket.CloseMessage, websocket.FormatCloseMessage(websocket.CloseNormalClosure, "")); err != nil {
-				log.Println("write close:", err)
-				return
-			}
-			select {
-			case <-done:
-			case <-time.After(1 * time.Second):
-			}
-			return
-		}
+	select {
+	case <-pong:
+		log.Println("pong!")
+		close(pong)
+		return nil
+	case err := <-ws.err:
+		log.Println("err!")
+		return err
+	case <-time.After(srvTimeout):
+		err := errors.New("timeout waiting for PING from server")
+		log.Println("[ERR]", err)
+		return err
 	}
 }
