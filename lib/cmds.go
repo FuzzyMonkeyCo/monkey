6e327e18c6529c53ede309ebf5595c66336d5d65
Pierre Fenoll <pierrefenoll@gmail.com>
2018-12-25 13:25:34 +0100

    some cleanup
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- lib/cmds.go
+++ lib/cmds.go
@@ -1,10 +1,5 @@
 package lib
 
-import (
-	"encoding/json"
-	"fmt"
-)
-
 var (
 	lastLane      lane
 	shrinkingFrom lane
@@ -18,59 +13,6 @@ type lane struct {
 
 type Action interface {
 	exec(mnk *Monkey) (act Action, err error)
-}
-
-type cmdKind int
-
-const (
-	kindDone cmdKind = iota
-	kindReq
-	kindReset
-	kindStart
-	kindStop
-)
-
-func (k cmdKind) String() string {
-	return map[cmdKind]string{
-		kindReq:   "req",
-		kindReset: "reset",
-		kindStart: "start",
-		kindStop:  "stop",
-		kindDone:  "done",
-	}[k]
-}
-
-func (k *cmdKind) UnmarshalJSON(data []byte) (err error) {
-	var cmd string
-	if err = json.Unmarshal(data, &cmd); err != nil {
-		return
-	}
-
-	if kind, ok := map[string]cmdKind{
-		"req":   kindReq,
-		"reset": kindReset,
-		"done":  kindDone,
-	}[cmd]; ok {
-		*k = kind
-		return
-	}
-	err = fmt.Errorf("expected one of req reset done, not %s", cmd)
-	return
-}
-
-func (k cmdKind) MarshalJSON() (data []byte, err error) {
-	var ok bool
-	if data, ok = map[cmdKind][]byte{
-		kindReq:   []byte(`"req"`),
-		kindReset: []byte(`"reset"`),
-		kindStart: []byte(`"start"`),
-		kindStop:  []byte(`"stop"`),
-		kindDone:  []byte(`"done"`),
-	}[k]; ok {
-		return
-	}
-	err = fmt.Errorf("impossibru %#v", k)
-	return
 }
 
 func (act *RepValidateProgress) exec(mnk *Monkey) (nxt Action, err error) {
