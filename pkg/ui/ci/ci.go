33bdcfcaf772fff088775a6b4d5daa8f016390ed
Pierre Fenoll <pierrefenoll@gmail.com>
2020-01-11 17:13:11 +0100

    nit
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- pkg/ui/ci/ci.go
+++ pkg/ui/ci/ci.go
@@ -7,30 +7,23 @@ import (
 	"github.com/FuzzyMonkeyCo/monkey/pkg/ui"
 )
 
-const (
-	cTest  = "●"
-	cCall  = "•"
-	cCheck = "."
-)
-
 var _ ui.Progresser = (*Progresser)(nil)
 
 type Progresser struct {
 	totalTestsCount, totalCallsCount, totalChecksCount uint32
 }
 
-func dot(s string, n uint32, o *uint32) {
+func dot(n uint32, o *uint32) {
 	if *o != n {
-		fmt.Printf(s)
 		*o = n
 	}
 }
 
 func (p *Progresser) MaxTestsCount(v uint32)    {}
 func (p *Progresser) Terminate() error          { return nil }
-func (p *Progresser) TotalTestsCount(v uint32)  { dot(cTest, v, &p.totalTestsCount) }
-func (p *Progresser) TotalCallsCount(v uint32)  { dot(cCall, v, &p.totalCallsCount) }
-func (p *Progresser) TotalChecksCount(v uint32) { dot(cCheck, v, &p.totalChecksCount) }
+func (p *Progresser) TotalTestsCount(v uint32)  { dot(v, &p.totalTestsCount) }
+func (p *Progresser) TotalCallsCount(v uint32)  { dot(v, &p.totalCallsCount) }
+func (p *Progresser) TotalChecksCount(v uint32) { dot(v, &p.totalChecksCount) }
 func (p *Progresser) TestCallsCount(v uint32)   {}
 func (p *Progresser) CallChecksCount(v uint32)  {}
 
@@ -42,16 +35,19 @@ func (p *Progresser) ChecksPassed() {
 }
 
 func (p *Progresser) CheckPassed(s string) {
-	fmt.Println(" " + as.ColorOK.Sprintf("PASSED") + " " + as.ColorNFO.Sprintf(s))
+	as.ColorOK.Printf("PASSED ")
+	as.ColorNFO.Println(s)
 }
 
 func (p *Progresser) CheckSkipped(s string) {
-	fmt.Println(" " + as.ColorWRN.Sprintf("SKIPPED") + " " + as.ColorNFO.Sprintf(s) + " skipped")
+	as.ColorWRN.Printf("SKIPPED ")
+	as.ColorNFO.Println(s)
 }
 
 func (p *Progresser) CheckFailed(ss []string) {
 	if len(ss) > 0 {
-		fmt.Println(" " + as.ColorERR.Sprintf("FAILED") + " " + as.ColorNFO.Sprintf(ss[0]))
+		as.ColorERR.Printf("FAILED ")
+		as.ColorNFO.Println(ss[0])
 	}
 	if len(ss) > 1 {
 		for _, s := range ss[1:] {
