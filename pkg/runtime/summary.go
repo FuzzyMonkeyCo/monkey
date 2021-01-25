0e504884a18da29e7394637cff80600b343bef5a
Pierre Fenoll <pierrefenoll@gmail.com>
2021-01-25 18:31:25 +0100

    cleanup
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- pkg/runtime/summary.go
+++ pkg/runtime/summary.go
@@ -8,8 +8,6 @@ type TestingCampaignOutcomer interface {
 
 var _ TestingCampaignOutcomer = (*TestingCampaignSuccess)(nil)
 var _ TestingCampaignOutcomer = (*TestingCampaignFailure)(nil)
-
-// var _ TestingCampaignOutcomer = (*TestingCampaignShrinkable)(nil)
 var _ TestingCampaignOutcomer = (*TestingCampaignFailureDueToResetterError)(nil)
 
 // TestingCampaignSuccess indicates no bug was found during fuzzing.
@@ -18,32 +16,18 @@ type TestingCampaignSuccess struct{}
 // TestingCampaignFailure indicates a bug was found during fuzzing.
 type TestingCampaignFailure struct{}
 
-// // TestingCampaignShrinkable indicates a bug-producing test can be shrunk.
-// type TestingCampaignShrinkable struct{}
-
 // TestingCampaignFailureDueToResetterError indicates a bug was found during reset.
 type TestingCampaignFailureDueToResetterError struct{}
 
 func (tc *TestingCampaignSuccess) Error() string { return "Found no bug" }
 func (tc *TestingCampaignFailure) Error() string { return "Found a bug" }
-
-// func (tc *TestingCampaignShrinkable) Error() string { return "Found a shrinkable bug" }
 func (tc *TestingCampaignFailureDueToResetterError) Error() string {
 	return "Something went wrong while resetting the system to a neutral state."
 }
 
-func (tc *TestingCampaignSuccess) isTestingCampaignOutcomer() {}
-func (tc *TestingCampaignFailure) isTestingCampaignOutcomer() {}
-
-// func (tc *TestingCampaignShrinkable) isTestingCampaignOutcomer()                {}
+func (tc *TestingCampaignSuccess) isTestingCampaignOutcomer()                   {}
+func (tc *TestingCampaignFailure) isTestingCampaignOutcomer()                   {}
 func (tc *TestingCampaignFailureDueToResetterError) isTestingCampaignOutcomer() {}
-
-// // summaryFrom reports to the user on the testing campaign
-// func (rt *Runtime) summaryFrom(r *fm.Srv_FuzzingResult) TestingCampaignOutcomer {
-// 	// 	shrinkable []uint32,
-// 	// 	noShrinking bool,
-
-// }
 
 func plural(s string, n uint32) string {
 	if n == 1 {
@@ -51,15 +35,3 @@ func plural(s string, n uint32) string {
 	}
 	return s + "s"
 }
-
-// func equalEIDs(a, b []uint32) bool {
-// 	if len(a) != len(b) {
-// 		return false
-// 	}
-// 	for i, x := range a {
-// 		if x != b[i] {
-// 			return false
-// 		}
-// 	}
-// 	return true
-// }
