defc6a53baac0bab0b2ba5d48043b7677d6e032f
Pierre Fenoll <pierrefenoll@gmail.com>
2021-02-18 16:48:36 +0100

    nit
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- pkg/starlarktruth/errors.go
+++ pkg/starlarktruth/errors.go
@@ -53,3 +53,13 @@ func (t *T) unhandled(name string, args ...starlark.Value) *UnhandledError {
 func (e UnhandledError) Error() string {
 	return fmt.Sprintf("unhandled .%s with %s for %s", e.name, e.actual.String(), e.args.String())
 }
+
+var _ error = (*IntegrityError)(nil)
+
+// IntegrityError describes that an `assert.that(actual)` was called but never any of its `.truth_methods(subject)`.
+// At the exception of `.named(name)` as by itself this still requires an assertion.
+type IntegrityError string
+
+func (e IntegrityError) Error() string {
+	return fmt.Sprintf("%s: %s.that(...) is missing an assertion", string(e), Default)
+}
