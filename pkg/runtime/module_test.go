f63563ba7892fe27f546ab52e09b6f0372645395
Pierre Fenoll <pierrefenoll@gmail.com>
2022-02-25 00:56:30 +0100

    require.IsIncreasing
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- pkg/runtime/module_test.go
+++ pkg/runtime/module_test.go
@@ -1,19 +1,17 @@
 package runtime
 
 import (
-	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/require"
 )
 
 func TestMonkeyModuleAttrsCount(t *testing.T) {
-	require.Len(t, (*module)(nil).AttrNames(), moduleAttrs)
+	names := (*module)(nil).AttrNames()
+	require.Len(t, names, moduleAttrs)
 }
 
 func TestMonkeyModuleAttrsNamesAreInOrder(t *testing.T) {
 	names := (*module)(nil).AttrNames()
-	sort.Strings(names)
-
-	require.Equal(t, names, (*module)(nil).AttrNames())
+	require.IsIncreasing(t, names)
 }
