f29a99231adb7dda0f1266fb5b8692770a1cf36a
Pierre Fenoll <pierrefenoll@gmail.com>
2023-08-18 19:27:57 +0200

    nits
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- pkg/resetter/shell/singleton.go
+++ pkg/resetter/shell/singleton.go
@@ -19,13 +19,13 @@ import (
 )
 
 const (
-	stdeitherPrefixSkip       = "+ "
-	stdeitherPrefixDropPrefix = "++ "
-	comPrefix                 = "###:" //FIXME: pick from random alnum at first run
-	comExec                   = comPrefix + "exec:"
-	comExit                   = comPrefix + "exit:"
-	comExitcode               = comPrefix + "exitcode:"
-	comUnexpected             = comPrefix + "unexpected:"
+	prefixSkip       = "+ "
+	prefixDropPrefix = "++ "
+	comPrefix        = "###:" //FIXME: pick from random alnum at first run
+	comExec          = comPrefix + "exec:"
+	comExit          = comPrefix + "exit:"
+	comExitcode      = comPrefix + "exitcode:"
+	comUnexpected    = comPrefix + "unexpected:"
 )
 
 func (s *Resetter) signal(verb, param string) {
@@ -152,6 +152,8 @@ func (s *Resetter) exec(ctx context.Context, shower progresser.Shower, envRead m
 				if x := bytes.TrimPrefix(data, []byte(comExitcode)); n != len(x) {
 					if y, err := strconv.ParseInt(string(x), 10, 8); err == nil {
 						s.rcoms <- uint8(y)
+					} else {
+						log.Println("[ERR] expected base10 int:", err)
 					}
 				}
 				if x := bytes.TrimPrefix(data, []byte(comUnexpected)); n != len(x) {
@@ -160,41 +162,27 @@ func (s *Resetter) exec(ctx context.Context, shower progresser.Shower, envRead m
 			}
 		})
 
-		stdout := newlinesWriter(func(data []byte) {
-			if n := len(data); n > 0 {
-				if bytes.HasPrefix(data, []byte("+ ")) {
-					return
-				}
-				if x := bytes.TrimPrefix(data, []byte("++ ")); n != len(x) {
-					if string(x) != "set +o xtrace" {
-						shower.Printf(" ❯ %s", x)
+		printer := func(p func(format string, args ...interface{})) io.Writer {
+			return newlinesWriter(func(data []byte) {
+				if n := len(data); n > 0 {
+					if bytes.HasPrefix(data, []byte(prefixSkip)) {
+						return
 					}
-					return
-				}
-				if x := bytes.TrimPrefix(data, []byte(comExitcode)); n != len(x) {
-					return
-				}
-				shower.Printf("%s", data)
-			}
-		})
-
-		stderr := newlinesWriter(func(data []byte) {
-			if n := len(data); n > 0 {
-				if bytes.HasPrefix(data, []byte("+ ")) {
-					return
-				}
-				if x := bytes.TrimPrefix(data, []byte("++ ")); n != len(x) {
-					if string(x) != "set +o xtrace" {
-						shower.Errorf(" ❯ %s", x)
+					if x := bytes.TrimPrefix(data, []byte(prefixDropPrefix)); n != len(x) {
+						if string(x) != "set +o xtrace" {
+							p(" ❯ %s", x)
+						}
+						return
 					}
-					return
+					if x := bytes.TrimPrefix(data, []byte(comExitcode)); n != len(x) {
+						return
+					}
+					p("%s", data)
 				}
-				if x := bytes.TrimPrefix(data, []byte(comExitcode)); n != len(x) {
-					return
-				}
-				shower.Errorf("%s", data)
-			}
-		})
+			})
+		}
+		stdout := printer(shower.Printf)
+		stderr := printer(shower.Errorf)
 
 		// TODO: mux stderr+stdout and fwd to server to track progress
 		exe.Stdout = io.MultiWriter(stdout, coms)
