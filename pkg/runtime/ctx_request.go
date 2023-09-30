c19fc2673369ab8a710bd80f90d4fe344ba19a34
Pierre Fenoll <pierrefenoll@gmail.com>
2023-09-30 10:04:06 +0200

    construct proto back from ctxreq
    Signed-off-by: Pierre Fenoll <pierrefenoll@gmail.com>

--- pkg/runtime/ctx_request.go
+++ pkg/runtime/ctx_request.go
@@ -2,9 +2,12 @@ package runtime
 
 import (
 	"fmt"
+	"log"
 	"strings"
 
 	"go.starlark.net/starlark"
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/types/known/structpb"
 
 	"github.com/FuzzyMonkeyCo/monkey/pkg/internal/fm"
 	"github.com/FuzzyMonkeyCo/monkey/pkg/starlarkvalue"
@@ -21,7 +24,8 @@ type ctxRequest_ struct {
 
 	method, url starlark.String
 	headers     *ctxHeader
-	body        starlark.Value
+	body        *structpb.Value //  starlark.Value FIXME
+	// FIXME: try starlark script that edits .body (as num and as dict/list, and as set)
 }
 
 func newCtxRequest(input *fm.Srv_Call_Input) *ctxRequest_ {
@@ -29,31 +33,51 @@ func newCtxRequest(input *fm.Srv_Call_Input) *ctxRequest_ {
 
 	case *fm.Srv_Call_Input_HttpRequest_:
 		r := input.GetHttpRequest()
-		var body starlark.Value
-		if x := r.GetBody(); x != nil {
-			body = starlarkvalue.FromProtoValue(x)
-		}
 		return &ctxRequest_{
 			ty:      ctxHttpRequest,
 			method:  starlark.String(r.GetMethod()),
 			url:     starlark.String(r.GetUrl()),
 			headers: newCtxHeader(r.GetHeaders()),
 			//content: absent as encoding will only happen later
-			body: body,
-		}, nil
+			body: r.GetBody(),
+		}
 
 	default:
-		panic(fmt.Errorf("unhandled output %T: %+v", x, input))
+		panic(fmt.Errorf("unhandled input %T: %+v", x, input))
 	}
 }
 
 func (cr *ctxRequest_) IntoProto(err error) *fm.Clt_CallRequestRaw {
-	input:=
-
 	var reason []string
-	if err != nil {
+	if len(reason) == 0 && err != nil {
 		reason = strings.Split(err.Error(), "\n")
 	}
+
+	input := func() *fm.Clt_CallRequestRaw_Input_HttpRequest_ {
+		switch cr.ty {
+		case "http_request":
+			var body []byte
+			if body, err = protojson.Marshal(cr.body); err != nil {
+				log.Println("[ERR]", err)
+				// return after sending msg
+				if len(reason) == 0 && err != nil {
+					reason = strings.Split(err.Error(), "\n")
+				}
+			}
+			return &fm.Clt_CallRequestRaw_Input_HttpRequest_{
+				HttpRequest: &fm.Clt_CallRequestRaw_Input_HttpRequest{
+					Method:      string(cr.method),
+					Url:         string(cr.url),
+					Headers:     cr.headers.IntoProto(),
+					Body:        body,
+					BodyDecoded: cr.body,
+				}}
+
+		default:
+			panic(fmt.Errorf("unhandled input %s: %+v", cr.ty, cr))
+
+		}
+	}()
 
 	return &fm.Clt_CallRequestRaw{
 		Input:  &fm.Clt_CallRequestRaw_Input{Input: input},
@@ -69,7 +93,7 @@ func (cr *ctxRequest_) Truth() starlark.Bool  { return true }
 func (cr *ctxRequest_) Type() string          { return cr.ty }
 
 func (cr *ctxRequest_) Freeze() {
-	cr.body.Freeze()
+	// cr.body.Freeze() FIXME
 	cr.headers.Freeze()
 }
 
@@ -89,7 +113,7 @@ func (cr *ctxRequest_) Attr(name string) (starlark.Value, error) {
 		if cr.body == nil {
 			return starlark.None, nil
 		}
-		return cr.body, nil
+		return starlarkvalue.FromProtoValue(cr.body), nil // FIXME: readonly
 	case "headers":
 		return cr.headers, nil
 	case "method":
